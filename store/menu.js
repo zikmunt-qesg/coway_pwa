b2b5b7b9eaef893f54bd1ebb3adc164d687498e8
zikmunt-qesg <60594409+zikmunt-qesg@users.noreply.github.com>
2020-05-14 10:29:24 +0900

    Create menu.js

---
+++ store/menu.js
@@ -0,0 +1,138 @@
+
+export const state = () => {
+    menu: [
+        {
+            title: 'Material Topic',
+            slug: '#',
+            child: [
+                {
+                    title: '제품과 서비스의 혁신',
+                    slug: '제품과-서비스의-혁신'
+                },
+                {
+                    title: '제품 책임',
+                    slug: '제품-책임'
+                },
+                {
+                    title: '데이터 및 개인정보보호',
+                    slug: '데이터-및-개인정보보호'
+                }
+            ]
+        },
+        {
+            title: 'Care for Sustainability',
+            link: '#',
+            child: [
+                {
+                    title: 'CEO 메시지',
+                    slug: 'CEO-메시지'
+                },
+                {
+                    title: '가치창출 프로세스',
+                    slug: '가치창출-프로세스'
+                },
+                {
+                    title: '이해관계자 참여 및 중요 주제',
+                    sulg: '이해관계자 참여 및 중요 주제'
+                },
+                {
+                    title: '지배구조',
+                    slug: '지배구조'
+                },
+                {
+                    title: '윤리경영',
+                    slug: '윤리경영'
+                },
+                {
+                    title: '리스크관리',
+                    slug: '리스크관리'
+                },
+                {
+                    title: '경제적가치 창출과 분배',
+                    slug: '경제적-가치-창출과-분배'
+                },
+            ]
+        },
+        {
+            title: 'Care for Environment',
+            link: '#',
+            child: [
+                {
+                    title: '깨끗한 물과 공기를 위한 환경경영',
+                    slug: '깨끗한-물과-공기를-위한-환경경영'
+                },
+                {
+                    title: '친환경 제품 개발과 제품 생애주기 관리',
+                    slug: '친환경-제품-개발과-제품-생애주기-관리'
+                },
+                {
+                    title: '기후변화 대응',
+                    slug: '기후변화-대응'
+                }
+            ]
+        },
+        {
+            title: 'Care for Society',
+            link: '#',
+            child:
+                [
+                    {
+                        title: '인재 육성 및 커뮤니케이션',
+                        slug: '인재-육성-및-커뮤니케이션'
+                    },
+                    {
+                        title: '다양성 기반의 기업문화와 안전한 근무환경',
+                        slug: '다양성-기반의-기업문화와-안전한-근무환경'
+                    },
+                    {
+                        title: '라이프케어 전문 파트너 양성',
+                        slug: '라이프케어-전문-파트너-양성'
+                    },
+                    {
+                        title: '동반성장',
+                        slug: '동반성장'
+                    },
+                    {
+                        title: '지역사회 참여',
+                        slug: '지역사회-참여'
+                    },
+                    {
+                        title: '협력회사 CSR 리포트',
+                        slug: '협력회사-CSR-리포트'
+                    },
+                    {
+                        title: '인권보호 리포트',
+                        slug: '인권보호-리포트'
+                    }
+                ]
+        },
+        {
+            title: 'Data and Frameworks',
+            slug: '#',
+            child:
+                [
+                    {
+                        title: '정책 및 이니셔티브',
+                        slug: '정책-및-이니셔티브'
+                    },
+                    {
+                        title: '데이터 센터',
+                        slug: '데이터-센터'
+                    },
+                    {
+                        title: '보고 프레임워크 인덱스',
+                        slug: '보고-프레임워크-인덱스'
+                    },
+                    {
+                        title: '보고 범위 및 검증',
+                        slug: '보고-범위-및-검증'
+                    },
+                    {
+                        title: 'Sustainability News',
+                        slug: 'Sustainabiilty-News'
+                    }
+                ]
+        }
+
+    ]
+}
