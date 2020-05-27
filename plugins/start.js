6f700b8a9d984526b8fb9ea238f750cc87ff5d9b
ikhyun bae <ihbae@qesg.co.kr>
2020-05-27 15:17:24 +0900

    search

--- plugins/start.js
+++ plugins/start.js
@@ -2,8 +2,28 @@ import Vue from 'vue'
 import GlobalComponent from '@/components/GlobalComponents/GlobalComponent'
 import DynamicRenderer from '@/components/GlobalComponents/DynamicRenderer'
 import h7 from '@/components/GlobalComponents/h7'
+import cssVars from 'css-vars-ponyfill'
 
 Vue.component('GlobalComponent', GlobalComponent)
 Vue.component('DynamicRenderer', DynamicRenderer)
 Vue.component('h7', h7)
 
+if (process.client){
+    cssVars({
+        rootElement: document,
+        shadowDOM: false,
+    
+        include: 'link[rel=stylesheet], style',
+        exclude: '',
+        variables: {},
+    
+        onlyLegacy: true,
+        preserveStatic: true,
+        preserveVars: false,
+        silent: false,
+        updateDOM: true,
+        updateURLs: true,
+        watch: false
+
+    })
+}
