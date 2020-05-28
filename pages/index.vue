853326c52c6b92f4a278a8d32be03b1144bad3d1
khj00200 <59757379+khj00200@users.noreply.github.com>
2020-05-28 15:59:56 +0900

    인터넷 연결 끊김

--- pages/index.vue
+++ pages/index.vue
@@ -6,34 +6,91 @@
     <b-container>
         <hr class="mb-4">
         <h1 class="text-center mb-4"> SUSTAINABILITY ISSUES 2019</h1>
-        <p class="text-center f-95"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.
+        <p class="text-center f-95 mb-4"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.
         </p>
         <b-row>
-            <b-col class="col-12 col-md-4 text-white">
-                <b-card class="bg-blue4 p-lg-2 mb-3 mb-md-0 h-100 main1-card">
-                    <div class="f-80 fw-300 main1-card-script"> 최근의 글로벌 사회·환경 문제는 소비자 생활패턴의 변화를 이끌며 환경가전 시장에 큰 영향을 끼치고 있습니다. 코웨이는 제품과 서비스 혁신을 통해 건강과 위생은 물론 고객 개인의 라이프스타일을 반영한 새로운 가치를 창출합니다.
+            <b-col class="col-12 col-md-4 text-white mb-3 mb-md-0">
+                <b-card class="bg-blue4 p-lg-2 h-100 position-relative border-0">
+                    <div class="f-80 fw-300 position-absolute pr-4"> 
+                    최근의 글로벌 사회·환경 문제는 소비자 생활패턴의 변화를 이끌며 환경가전 시장에 큰 영향을 끼치고 있습니다. 코웨이는 제품과 서비스 혁신을 통해 건강과 위생은 물론 고객 개인의 라이프스타일을 반영한 새로운 가치를 창출합니다.
                     </div>
-                    <div class="mb-3 mb-md-5"></div>
-                    <div style="bottom:0;"><b-img src="https://picsum.photos/400/400/?image=20" rounded="circle" class="main1-card-img"></b-img></div>
+                    <div class="my-5 my-md-4 my-md-5 py-4 py-sm-2 py-md-4"></div>
+                    <div class="px-2 mb-4 border border-white">제품과 서비스의 혁신</div>
+                    <b-img src="https://picsum.photos/400/400/?image=20" rounded="circle" class="main1-card-img mt-2 mb-3"></b-img>
                 </b-card>
             </b-col>
-            <b-col class="col-12 col-md-4 text-white">
-                <b-card class="bg-blue7 p-lg-2 mb-3 mb-md-0 h-100 main1-card">
-                    <div class="f-80 fw-300 main1-card-script"> 최근의 글로벌 사회·환경 문제는 소비자 생활패턴의 변화를 이끌며 환경가전 시장에 큰 영향을 끼치고 있습니다. 코웨이는 제품과 서비스 혁신을 통해 건강과 위생은
+            <b-col class="col-12 col-md-4 text-white mb-3 mb-md-0">
+                <b-card class="bg-blue7 p-lg-2 h-100 position-relative border-0">
+                    <div class="f-80 fw-300 position-absolute pr-4"> 
+                    최근의 글로벌 사회·환경 문제는 소비자 생활패턴의 변화를 이끌며 환경가전 시장에 큰 영향을 끼치고 있습니다. 코웨이는 제품과 서비스 혁신을 통해 건강과 위생은
                     </div>
-                    <div class="mb-3 mb-md-5"></div>
-                    <div style="bottom:0;"><b-img src="https://picsum.photos/400/400/?image=20" rounded="circle" class="main1-card-img"></b-img></div>
+                    <div class="my-5 my-md-4 my-md-5 py-4 py-sm-2 py-md-4"></div>
+                    <div class="px-2 mb-4 border border-white">제품 책임</div>
+                    <b-img src="https://picsum.photos/400/400/?image=20" rounded="circle" class="main1-card-img mt-2 mb-3"></b-img>
                 </b-card>
             </b-col>
-            <b-col class="col-12 col-md-4 text-white">
-                <b-card class="bg-blue3 p-lg-2 mb-3 mb-md-0 h-100 main1-card">
-                    <div class="f-80 fw-300 main1-card-script"> 최근의 글로벌 사회·환경 문제는 소비자 생활패턴의 변화를 이끌며 환경가전 시장에 큰 영향을 끼치고 있습니다. 코웨이는 제품과 서비스 혁신을 통해 건강과 위생은 물론 고객
+            <b-col class="col-12 col-md-4 text-white mb-3 mb-md-0">
+                <b-card class="bg-blue3 p-lg-2 h-100 position-relative border-0">
+                    <div class="f-80 fw-300 position-absolute pr-4"> 
+                    최근의 글로벌 사회·환경 문제는 소비자 생활패턴의 변화를 이끌며 환경가전 시장에 큰 영향을 끼치고 있습니다. 코웨이는 제품과 서비스 혁신을 통해 건강과 위생은 물론 고객
                     </div>
-                    <div class="mb-3 mb-md-5"></div>
-                    <div style="bottom:0;"><b-img src="https://picsum.photos/400/400/?image=20" rounded="circle" class="main1-card-img"></b-img></div>
+                    <div class="my-5 my-md-4 my-md-5 py-4 py-sm-2 py-md-4"></div>
+                    <div class="px-2 mb-4 border border-white">데이터 및 개인정보 보호</div>
+                    <b-img src="https://picsum.photos/400/400/?image=20" rounded="circle" class="main1-card-img mt-2 mb-3"></b-img>
                 </b-card>
             </b-col>
         </b-row>
+        <hr class="mb-5">
+        <h1 class="text-center mb-4"> KEY FIGURES 2019</h1>
+        <p class="text-center f-95 mb-4"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.
+        </p>
+        <div class="col-12 bg-gray3 fw-500 py-2 px-3"> Economic Performance </div>
+        <b-row class="my-4">
+            <b-col class="col-12 col-md-4 border-right-gray">
+                <div class="mb-3 blue8 fw-400"> 코웨이는 물과 공기를 깨끗하게 하는 ‘본질’에 집중하며, 고객 경험과 서비스의 가치를 향상시킬 수 있는 끊임없는 혁신으로 지속성장해가고 있습니다. 
+                </div>
+                <h7 class="blue8 fw-500"> 매출액 </h7>
+                <div class="temp-icon7"></div>
+            </b-col>
+            <b-col class="col-12 col-md-4 border-right-gray">
+                <div class="temp-icon7"></div>
+                <div class="blue8 fw-400 mt-4"> 제품과 서비스 혁신 </div>
+                <div class="blue8 fw-400 f-110"> 본질적 가치를 높이는 제품과 서비스 혁신으로 CES에서 5년 연속 혁신상을 수상했습니다. 
+                </div>
+            </b-col>
+            <b-col class="col-12 col-md-4">
+                <div class="d-flex justify-content-between align-items-start">
+                    <div class="blue8" style="font-size: 70px; margin-top: -20px;">95.7%</div>
+                    <div class="temp-icon5"></div>
+                </div>
+                <div class="mb-3 gray6 fw-400"> 2019년 말 기준 국내외 000 고객 계정을 보유한 코웨이는 오랜 기간 연구해온 데이터를 미래 기술과 접목하여 고객에게 보다 정확하고 편리한 새로운 서비스 경험을 제공합니다. 
+                </div>
+            </b-col>
+        </b-row>
+        <div class="col-12 bg-gray3 fw-500 py-2 px-3"> Economic Performance </div>
+        <b-row class="my-4">
+            <b-col class="col-12 col-md-4 border-right-gray">
+                <div class="mb-3 blue8 fw-400"> 코웨이는 물과 공기를 깨끗하게 하는 ‘본질’에 집중하며, 고객 경험과 서비스의 가치를 향상시킬 수 있는 끊임없는 혁신으로 지속성장해가고 있습니다. 
+                </div>
+                <h7 class="blue8 fw-500"> 매출액 </h7>
+                <div class="temp-icon7"></div>
+            </b-col>
+            <b-col class="col-12 col-md-4 border-right-gray">
+                <div class="temp-icon7"></div>
+                <div class="blue8 fw-400 mt-4"> 제품과 서비스 혁신 </div>
+                <div class="blue8 fw-400 f-110"> 본질적 가치를 높이는 제품과 서비스 혁신으로 CES에서 5년 연속 혁신상을 수상했습니다. 
+                </div>
+            </b-col>
+            <b-col class="col-12 col-md-4">
+                <div class="d-flex justify-content-between align-items-start">
+                    <div class="blue8" style="font-size: 70px; margin-top: -20px;">95.7%</div>
+                    <div class="temp-icon5"></div>
+                </div>
+                <div class="mb-3 gray6 fw-400"> 2019년 말 기준 국내외 000 고객 계정을 보유한 코웨이는 오랜 기간 연구해온 데이터를 미래 기술과 접목하여 고객에게 보다 정확하고 편리한 새로운 서비스 경험을 제공합니다. 
+                </div>
+            </b-col>
+        </b-row>
+
 
        
         <hr class="space-p75">
@@ -71,10 +128,23 @@ export default {
     z-index: -1;
     top: -20vw;
 }
+
 .main1-card-img{
-    width: 70%;
-    max-width: 170px;
+    height: 160px;
 }
+@media (max-width: 768px) {
+    .main1-card-img{
+        height: 140px;
+    }
+}
+
+// .main1-card-script {
+//     margin-top: 75%;
+// }
+// .main1-card-script > p {
+//     position: absolute;
+// }
+
 // .title {
 //   display: block;
 //   font-weight: bold;
