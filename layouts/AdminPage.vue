1c74c72c01a59d00e6eb2868dff05e4ea32df779
khj00200 <59757379+khj00200@users.noreply.github.com>
2020-06-01 17:52:50 +0900

    Admin chart 그리기 준비 완료

--- layouts/AdminPage.vue
+++ layouts/AdminPage.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
+<div>
     <script src="https://kit.fontawesome.com/3f3ed23357.js" crossorigin="anonymous"></script>
     <admin-header />
 
-    <b-row no-gutters class="d-flex align-items-stretch min-vh-100">
-        <b-col class="col-2 bg-gray6">
+    <b-row no-gutters class="d-flex align-items-stretch min-vh-80">
+        <b-col :class="$route.path!='/dashboard'? 'col-2 bg-gray6':''">
             <admin-side class="admin-side" />
         </b-col>
-        <b-col class="col-10 bg-gray2">
+        <b-col :class="$route.path!='/dashboard' ? 'col-10 bg-gray2' : 'col-12 bg-gray2'">
             <nuxt class="nuxt" />
-        </b-col>
-    </b-row>
+        </b-col>        
+    </b-row>    
 
     <site-footer />
-  </div>
+</div>
 </template>
 <script>
 import { mapState, mapActions } from 'vuex'
@@ -47,9 +47,9 @@ export default {
 </script>
 
 <style>
-/* .stretch {
-  min-height: 92vh;
-} */
+.min-vh-80 {
+  min-height: 80vh;
+}
 /* 
 .admin-side {
   background-color: #333333;
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,18 +1,36 @@
 <template>
-    <div class="fullbody min-vh-100">
-        코웨이 Sustainability 웹사이트 관리자 페이지 입니다.
-    </div>
+<div class="w-100 h-100 d-flex align-items-center">
+    <b-container>
+        <b-row>
+            <b-col class="col-0 col-md-4"></b-col>
+            <b-col class="col-12 col-md-4 px-4 px-md-0">
+                <div class="text-center fw-400 mb-4"> 관리자 페이지 접속을 위해  패스워드를 입력해주세요</div>
+                <b-form-input class=""></b-form-input>
+                <div>문의: info@qesg.co.kr / QESG(퀀티파이드이에스지)</div>
+            </b-col>
+            <b-col class="col-0 col-md-4"></b-col>
+        </b-row>
+    </b-container>         
+</div>
 </template>
 <script>
 export default {
-  layout: 'AdminPage'
+    layout: 'AdminPage',
+    data() {
+        return {
+            show_login: false,
+        }
+    },
+    created(){
+        this.show_login = true
+    }
 }
 </script>
 <style scoped>
-.fullbody {
+/* .fullbody {
   width: 100%;
   height: 92vh;
   text-align: center;
   line-height: 200px;
-}
+} */
 </style>
