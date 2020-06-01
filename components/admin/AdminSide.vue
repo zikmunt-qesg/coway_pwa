f293d931c6f6fd9922dbfd2c2f8a27a1394e30f2
khj00200 <59757379+khj00200@users.noreply.github.com>
2020-06-01 14:18:34 +0900

    Update AdminSide.vue

--- components/admin/AdminSide.vue
+++ components/admin/AdminSide.vue
@@ -3,14 +3,11 @@
     <b-list-group class="pt-4 mx-2">
         <b-list-group-item button @click="$router.push('/dashboard/analytics')" class="mb-2"
         :variant="active_page=='/dashboard/analytics' ? 'blue':'dark'"
-        >
-            페이지 뷰
+        > 페이지 뷰
         </b-list-group-item>
         <b-list-group-item button @click="$router.push('/dashboard/news_admin')"
         :variant="active_page=='/dashboard/news_admin' || active_page=='/dashboard/write_article' ? 'blue':'dark'"
-        
-        >
-            뉴스 관리
+        > 뉴스 관리
         </b-list-group-item>
     </b-list-group>
 </div>
@@ -35,9 +32,9 @@ export default {
     font-size: 1.1rem;
     background-color: $gray3;
     color: $gray7;
-    font-weight: 500;
+    font-weight: 400;
 }
-.list-group-item-blue{
+.list-group-item-blue, .list-group-item:focus{
     font-size: 1.1rem;
     background-color: $coway-blue!important;
     border: 0;
