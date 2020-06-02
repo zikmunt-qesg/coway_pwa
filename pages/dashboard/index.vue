dfbb4e93114e32fd6d630618a6f09b75e8a31daf
khj00200 <59757379+khj00200@users.noreply.github.com>
2020-06-02 10:41:40 +0900

    수정 완료

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -5,17 +5,21 @@
             <b-col class="col-0 col-sm-2 col-lg-3"></b-col>
             <b-col class="col-12 col-sm-8 col-lg-6 px-4 px-md-0 text-center">
                 <div class="fw-400 mb-4"> 관리자 페이지 접속을 위해  패스워드를 입력해주세요</div>
-                <div class="d-flex justify-content-center"> 
-                    <b-form-input v-model="password" type="password" placeholder="password" class="password-form" @keyup.enter="login"></b-form-input>
-                    <b-button @click="login">로그인</b-button>
-                </div>
-                <!-- <b-overlay :show="this.login_status=='login_try'" opacity="0.4">
+                
+                <b-overlay :show="this.login_status=='login_try'" opacity="0.4">
                     <template v-slot:overlay>
-
+                        <div class="d-flex align-items-center">
+                            <b-spinner variant="secondary"></b-spinner>
+                        </div>
                     </template>
-                </b-overlay> -->
-                {{ msg }}
-                <div class="mt-4 f-90">문의: info@qesg.co.kr / QESG(퀀티파이드이에스지)</div>
+                    <div class="d-flex justify-content-center"> 
+                        <b-form-input v-model="password" type="password" placeholder="password" class="password-form" @keyup.enter="login"></b-form-input>
+                        <b-button @click="login">로그인</b-button>
+                    </div>
+                </b-overlay>
+
+                <div :class="[login_status=='login_fail'? 'red':'','mt-2 f-90']"> {{msg}} </div>
+                <div v-if="login_status=='login_fail'" class="mt-4 f-90">문의: info@qesg.co.kr / QESG(퀀티파이드이에스지)</div>
             </b-col>
             <b-col class="col-0 col-sm-2 col-lg-3"></b-col>
         </b-row>
@@ -48,14 +52,22 @@ export default {
     },
     watch:{
         login_status(newVal) {
-                if(process.client){
-                    if(newVal=='login_try'){
-                        this.msg = '로그인 중입니다'
-                    }
-                    else if(newVal=='login_fail'){
-                        this.msg = '로그인에 실패하였습니다. 비밀번호를 다시 확인하여 주십시오.'
-                    }
+            if(process.client){
+                if(newVal=='login_try'){
+                    this.msg = '로그인 중입니다'
                 }
+                else if(newVal=='login_fail'){
+                    this.msg = '로그인에 실패하였습니다. 비밀번호를 다시 확인하여 주십시오.'
+                }
+            }
+        },
+        password(newVal){
+            if(process.client){
+                if(newVal.length==0){
+                    this.msg = ''
+                    this.login_status = ''
+                }
+            }
         }
     }
     // created(){
