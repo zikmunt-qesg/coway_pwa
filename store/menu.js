d562df1b2477076922560eb1f402ca57ada1a985
khj00200 <59757379+khj00200@users.noreply.github.com>
2020-06-24 16:40:23 +0900

    Update menu.js

--- store/menu.js
+++ store/menu.js
@@ -13,15 +13,15 @@ export const state = () => ({
                         [
                             {
                                 title: '본질적 가치를 높이는 제품 혁신',
-                                link: '/static_page/Product-and-Service-Innovation#1'
+                                link: '/static_page/Product-and-Service-Innovation#s1'
                             },
                             {
                                 title: '미래 기술 기반의 서비스 혁신',
-                                link: '/static_page/Product-and-Service-Innovation#2'
+                                link: '/static_page/Product-and-Service-Innovation#s2'
                             },
                             {
                                 title: '혁신을 위한 연구개발 역량 강화',
-                                link: '/static_page/Product-and-Service-Innovation#3'
+                                link: '/static_page/Product-and-Service-Innovation#s3'
                             },
 
                         ]
@@ -33,15 +33,15 @@ export const state = () => ({
                         [
                             {
                                 title: '제품 책임 거버넌스',
-                                link: '/static_page/Product-Responsibility#1'
+                                link: '/static_page/Product-Responsibility#s1'
                             },
                             {
                                 title: '제품 신뢰성 강화',
-                                link: '/static_page/Product-Responsibility#2'
+                                link: '/static_page/Product-Responsibility#s2'
                             },
                             {
                                 title: '안전 인증 확대 및 고객 안심',
-                                link: '/static_page/Product-Responsibility#3'
+                                link: '/static_page/Product-Responsibility#s3'
                             }
                         ]
                 },
@@ -52,15 +52,15 @@ export const state = () => ({
                         [
                             {
                                 title: '정보보안 및 개인정보보호 원칙과 조직',
-                                link: '/static_page/Information-Security-and-Personal-Information-Protection#1'
+                                link: '/static_page/Information-Security-and-Personal-Information-Protection#s1'
                             },
                             {
                                 title: '정보보호 관리체계',
-                                link: '/static_page/Information-Security-and-Personal-Information-Protection#2'
+                                link: '/static_page/Information-Security-and-Personal-Information-Protection#s2'
                             },
                             {
                                 title: '모니터링 및 실사 프로세스',
-                                link: '/static_page/Information-Security-and-Personal-Information-Protection#3'
+                                link: '/static_page/Information-Security-and-Personal-Information-Protection#s3'
                             }
                         ]
                 }
@@ -82,11 +82,11 @@ export const state = () => ({
                         [
                             {
                                 title: '비즈니스 모델',
-                                link: '/static_page/Value-Creation-Story#1'
+                                link: '/static_page/Value-Creation-Story#s1'
                             },
                             {
                                 title: '비즈니스 밸류체인',
-                                link: '/static_page/Value-Creation-Story#2'
+                                link: '/static_page/Value-Creation-Story#s2'
                             }
                         ]
                 },
@@ -97,15 +97,15 @@ export const state = () => ({
                         [
                             {
                                 title: '이해관계자 참여',
-                                link: '/static_page/Stakeholder-Engagement-and-Material-Issues#1'
+                                link: '/static_page/Stakeholder-Engagement-and-Material-Issues#s1'
                             },
                             {
                                 title: '중대성 평가',
-                                link: '/static_page/Stakeholder-Engagement-and-Material-Issues#2'
+                                link: '/static_page/Stakeholder-Engagement-and-Material-Issues#s2'
                             },
                             {
                                 title: '중대성 맥락과 중장기 목표',
-                                link: '/static_page/Stakeholder-Engagement-and-Material-Issues#3'
+                                link: '/static_page/Stakeholder-Engagement-and-Material-Issues#s3'
                             }
                         ]
                 },
@@ -116,23 +116,15 @@ export const state = () => ({
                         [
                             {
                                 title: '이사회 중심의 책임경영 수행',
-                                link: '/static_page/Corporate-Governance#1'
-                            },
-                            {
-                                title: '이사회의 독립성 및 전문성',
-                                link: '/static_page/Corporate-Governance#2'
-                            },
-                            {
-                                title: '이사회 산하 위원회 운영',
-                                link: '/static_page/Corporate-Governance#3'
+                                link: '/static_page/Corporate-Governance#s1'
                             },
                             {
                                 title: '성과평가와 보수 지급',
-                                link: '/static_page/Corporate-Governance#4'
+                                link: '/static_page/Corporate-Governance#s2'
                             },
                             {
                                 title: '소유구조 및 의결권',
-                                link: '/static_page/Corporate-Governance#5'
+                                link: '/static_page/Corporate-Governance#s3'
                             }
                         ]
                 },
@@ -143,15 +135,15 @@ export const state = () => ({
                         [
                             {
                                 title: '윤리강령',
-                                link: '/static_page/Ethics-Management#1'
+                                link: '/static_page/Ethics-Management#s1'
                             },
                             {
                                 title: '윤리진단',
-                                link: '/static_page/Ethics-Management#2'
+                                link: '/static_page/Ethics-Management#s2'
                             },
                             {
                                 title: '윤리교육',
-                                link: '/static_page/Ethics-Management#3'
+                                link: '/static_page/Ethics-Management#s3'
                             }
                         ]
                 },
@@ -162,11 +154,11 @@ export const state = () => ({
                         [
                             {
                                 title: '전사적 리스크 관리 체계',
-                                link: '/static_page/Risk-Management#1'
+                                link: '/static_page/Risk-Management#s1'
                             },
                             {
                                 title: '잠재 리스크의 관리',
-                                link: '/static_page/Risk-Management#2'
+                                link: '/static_page/Risk-Management#s2'
                             }
                         ]
                 },
@@ -177,15 +169,15 @@ export const state = () => ({
                         [
                             {
                                 title: '경제적 가치 창출',
-                                link: '/static_page/Economic-Value-Creation-and-Distribution#1'
+                                link: '/static_page/Economic-Value-Creation-and-Distribution#s1'
                             },
                             {
                                 title: '경제적 가치 분배',
-                                link: '/static_page/Economic-Value-Creation-and-Distribution#2'
+                                link: '/static_page/Economic-Value-Creation-and-Distribution#s2'
                             },
                             {
                                 title: '조세 정책',
-                                link: '/static_page/Economic-Value-Creation-and-Distribution#3'
+                                link: '/static_page/Economic-Value-Creation-and-Distribution#s3'
                             }
                         ]
                 },
@@ -203,11 +195,11 @@ export const state = () => ({
                         [
                             {
                                 title: '환경경영을 통한 깨끗함의 가치 실현',
-                                link: '/static_page/Environmental-Management-for-Clean-Water-and-Air#1'
+                                link: '/static_page/Environmental-Management-for-Clean-Water-and-Air#s1'
                             },
                             {
                                 title: '가치사슬 전체의 환경역량 강화',
-                                link: '/static_page/Environmental-Management-for-Clean-Water-and-Air#2'
+                                link: '/static_page/Environmental-Management-for-Clean-Water-and-Air#s2'
                             }
                         ]
                 },
@@ -218,11 +210,11 @@ export const state = () => ({
                         [
                             {
                                 title: '친환경 제품과 서비스, 비즈니스 설계',
-                                link: '/static_page/Eco-Friendly-Product-Development-and-Product-Life-Cycle-Management#1'
+                                link: '/static_page/Eco-Friendly-Product-Development-and-Product-Life-Cycle-Management#s1'
                             },
                             {
                                 title: '자원효율성 향상과 순환경제 지원',
-                                link: '/static_page/Eco-Friendly-Product-Development-and-Product-Life-Cycle-Management#2'
+                                link: '/static_page/Eco-Friendly-Product-Development-and-Product-Life-Cycle-Management#s2'
                             }
                         ]
                 },
@@ -233,19 +225,19 @@ export const state = () => ({
                         [
                             {
                                 title: '기후변화 목표 및 시나리오 관리',
-                                link: '/static_page/Responses-to-Climate-Change#1'
+                                link: '/static_page/Responses-to-Climate-Change#s1'
                             },
                             {
                                 title: '기후변화의 위험과 기회 분석',
-                                link: '/static_page/Responses-to-Climate-Change#2'
+                                link: '/static_page/Responses-to-Climate-Change#s2'
                             },
                             {
                                 title: '협력사 탄소 파트너십',
-                                link: '/static_page/Responses-to-Climate-Change#3'
+                                link: '/static_page/Responses-to-Climate-Change#s3'
                             },
                             {
                                 title: '온실가스 · 에너지 및 기타 환경 데이터',
-                                link: '/static_page/Responses-to-Climate-Change#4'
+                                link: '/static_page/Responses-to-Climate-Change#s4'
                             }
                         ]
                 }
@@ -264,15 +256,15 @@ export const state = () => ({
                             [
                                 {
                                     title: '착한성장을 위한 역량 강화',
-                                    link: '/static_page/Employee-Centered-Company-Culture-and-Safe-Working-Environment#1'
+                                    link: '/static_page/Employee-Centered-Company-Culture-and-Safe-Working-Environment#s1'
                                 },
                                 {
                                     title: '커뮤니케이션 기반의 기업문화',
-                                    link: '/static_page/Employee-Centered-Company-Culture-and-Safe-Working-Environment#2'
+                                    link: '/static_page/Employee-Centered-Company-Culture-and-Safe-Working-Environment#s2'
                                 },
                                 {
                                     title: '안전하고 건강한 근무환경',
-                                    link: '/static_page/Employee-Centered-Company-Culture-and-Safe-Working-Environment#3'
+                                    link: '/static_page/Employee-Centered-Company-Culture-and-Safe-Working-Environment#s3'
                                 }
                             ]
                     },
@@ -283,11 +275,11 @@ export const state = () => ({
                             [
                                 {
                                     title: '파트너 전문성 강화',
-                                    link: '/static_page/Train-Lifecare-Professional-Partners#1'
+                                    link: '/static_page/Train-Lifecare-Professional-Partners#s1'
                                 },
                                 {
                                     title: '파트너 상생 및 커뮤니케이션',
-                                    link: '/static_page/Train-Lifecare-Professional-Partners#2'
+                                    link: '/static_page/Train-Lifecare-Professional-Partners#s2'
                                 }
                             ]
                     },
@@ -298,11 +290,11 @@ export const state = () => ({
                             [
                                 {
                                     title: '협력사와의 소통을 통한 성장 지원',
-                                    link: '/static_page/Win-Win#1'
+                                    link: '/static_page/Win-Win#s1'
                                 },
                                 {
                                     title: '공정거래 문화 조성',
-                                    link: '/static_page/Win-Win#2'
+                                    link: '/static_page/Win-Win#s2'
                                 }
                             ]
                     },
@@ -313,15 +305,15 @@ export const state = () => ({
                             [
                                 {
                                     title: '코웨이 다운 사회공헌',
-                                    link: '/static_page/Local-Community#1'
+                                    link: '/static_page/Local-Community#s1'
                                 },
                                 {
                                     title: '깨끗한 물과 공기를 위한 코웨이의 약속',
-                                    link: '/static_page/Local-Community#2'
+                                    link: '/static_page/Local-Community#s2'
                                 },
                                 {
                                     title: '공공정책 참여',
-                                    link: '/static_page/Local-Community#3'
+                                    link: '/static_page/Local-Community#s3'
                                 }
                             ]
                     },
@@ -332,15 +324,15 @@ export const state = () => ({
                             [
                                 {
                                     title: '협력사 행동 규범',
-                                    link: '/static_page/Supplier-CSR-Report#1'
+                                    link: '/static_page/Supplier-CSR-Report#s1'
                                 },
                                 {
                                     title: '협력사 지속가능성 평가',
-                                    link: '/static_page/Supplier-CSR-Report#2'
+                                    link: '/static_page/Supplier-CSR-Report#s2'
                                 },
                                 {
                                     title: '분쟁광물',
-                                    link: '/static_page/Supplier-CSR-Report#3'
+                                    link: '/static_page/Supplier-CSR-Report#s3'
                                 }
                             ]
                     },
@@ -351,11 +343,11 @@ export const state = () => ({
                             [
                                 {
                                     title: '인권경영 원칙 및 가이드',
-                                    link: '/static_page/Human-Rights-Report#1'
+                                    link: '/static_page/Human-Rights-Report#s1'
                                 },
                                 {
                                     title: '인권 리스크 식별 및 완화 프로세스',
-                                    link: '/static_page/Human-Rights-Report#2'
+                                    link: '/static_page/Human-Rights-Report#s2'
                                 }
                             ]
                     }
