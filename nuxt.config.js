ebbff589480b1c5a0d2ee06e468c16b898cfdde5
zikmunt-qesg <60594409+zikmunt-qesg@users.noreply.github.com>
2020-07-01 14:02:44 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge'},
       {
         hid: 'description',
         name: 'description',
