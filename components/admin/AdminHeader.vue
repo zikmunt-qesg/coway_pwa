92a7c4f9d27a70a59ee0dde1e85b8f3c70ca46cb
zikmunt-qesg <60594409+zikmunt-qesg@users.noreply.github.com>
2020-07-01 21:27:22 +0900

    Update AdminHeader.vue

--- components/admin/AdminHeader.vue
+++ components/admin/AdminHeader.vue
@@ -1,13 +1,15 @@
 <template>
 <header>
     <div class="position-relative">
-        <b-navbar class="px-3 bg-gray7">
+        <b-navbar class="d-flex justify-content-between px-3 bg-gray7">
             <b-navbar-brand>
                 <nuxt-link to="/dashboard">
-                    <b-img class="header-logo" src="@/assets/images/coway-ci.svg" alt="Logo" />
+                    <b-img class="header-logo" src="@/assets/images/coway-ci.svg" alt="Logo" /><span class="f-90 gray3 fw-300"> Sustainability Website 관리자 페이지</span>
                 </nuxt-link>
             </b-navbar-brand>
-            <span class="f-90 gray3 fw-300"> Sustainability Website 관리자 페이지</span>
+            <b-nav-item to="/">
+                메인 페이지 바로가기
+            </b-nav-item>
         </b-navbar>
     </div>
 </header>
