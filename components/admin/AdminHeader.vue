8fccc9ec9b7e25983ef13dfb219e7bcce53b4d64
zikmunt-qesg <60594409+zikmunt-qesg@users.noreply.github.com>
2020-07-02 15:51:25 +0900

    Update AdminHeader.vue

--- components/admin/AdminHeader.vue
+++ components/admin/AdminHeader.vue
@@ -4,16 +4,32 @@
         <b-navbar class="d-flex justify-content-between px-3 bg-gray7">
             <b-navbar-brand>
                 <nuxt-link to="/dashboard">
-                    <b-img class="header-logo" src="@/assets/images/coway-ci.svg" alt="Logo" /><span class="f-90 gray3 fw-300"> Sustainability Website 관리자 페이지</span>
+                    <b-img class="header-logo" src="@/assets/images/coway-ci.svg" alt="Logo" /><span class="f-90 gray3 fw-300">Sustainability Website 관리자 페이지</span>
                 </nuxt-link>
             </b-navbar-brand>
-            <nuxt-link to="/" class="px-2 mr-2">
+            <nuxt-link v-if="is_IE!=true" to="/" class="px-2 mr-2">
                 메인 페이지 바로가기
             </nuxt-link>
         </b-navbar>
     </div>
 </header>
 </template>
+<script>
+export default {
+    computed:{
+        is_IE(){
+            if ( (navigator.appName == 'Netscape' && navigator.userAgent.toLowerCase().indexOf('trident') != -1) || (navigator.userAgent.toLowerCase().indexOf("msie") != -1) )
+            {
+                return true
+            }
+            else
+            {
+                return false
+            }
+        }
+    }
+}
+</script>
 
 <style scoped>
 
