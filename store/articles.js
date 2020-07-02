b91b78f07b79003cfc293edb55039d0c24469e7d
zikmunt-qesg <60594409+zikmunt-qesg@users.noreply.github.com>
2020-07-02 12:25:00 +0900

    async

--- store/articles.js
+++ store/articles.js
@@ -59,7 +59,12 @@ export const actions = {
             })
             .catch(error => {
                 console.log(error)
+<<<<<<< Updated upstream
             })       
+=======
+            })
+        
+>>>>>>> Stashed changes
         return await dispatch('readArticles')
     },
     deleteArticle({ state, commit, rootState }, { id }){
