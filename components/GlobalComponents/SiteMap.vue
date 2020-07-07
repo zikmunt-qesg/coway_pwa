6e1174a5970f1c6abcf76a7d49d799f525457e56
khj00200 <59757379+khj00200@users.noreply.github.com>
2020-07-07 14:29:06 +0900

    sitemap 영문 조정

--- components/GlobalComponents/SiteMap.vue
+++ components/GlobalComponents/SiteMap.vue
@@ -1,9 +1,7 @@
 <template>
 <div class="position-relative d-none d-lg-block" style="background-color: rgba(245,245,245,0.95)">
-    <b-container>
+    <b-container v-if="this.$store.state.is_ENG==false">
         <b-row class="pt-3 pb-3 font-noto d-flex flex-column flex-md-row">
-            <!-- <b-col class="col-0 col-lg-1"></b-col> -->
-            <!-- <b-col class="col-4 col-lg-1 f-80 gray5 cursor hover-underline px-xl-4 mb-3 mb-sm-0" @click.stop="$router.push('/')" style="height:50px;"> </b-col> -->
             <b-col class="" v-for="item in menu" :key="item.id" style="min-width: 192px;">
                 <div class="f-90 mb-3" @click.stop="toggleSiteMap">
                     <nuxt-link :to="`${item.link}`"><span class="gray8">{{ item.title }}</span></nuxt-link>
@@ -15,11 +13,24 @@
                     </div>
                 </div>
             </b-col>
-            <!-- <div style="margin-top:-1.75rem; margin-right: 1.25rem;"><a href="#" @click.stop="toggleSiteMap" class="close-btn f-280 fw-300"></a></div> -->
-            <!-- <b-col class="col-3 col-lg-1" style="margin-top:-1.6rem;"><a href="#" @click.stop="toggleSiteMap" class="close-btn f-280 fw-300"></a></b-col> -->
         </b-row>
     </b-container>
-    <!-- <a href="#" @click.stop="toggleSiteMap" class="close-btn py-0 f-280 fw-300 position-absolute" style="top:0; right:1em"></a> -->
+
+    <b-container v-else>
+        <b-row class="pt-3 pb-3 font-noto d-flex flex-column flex-md-row">
+            <b-col class="" v-for="item in menu" :key="item.id" style="min-width: 192px;">
+                <div class="f-90 mb-3" @click.stop="toggleSiteMap">
+                    <nuxt-link :to="`${item.link}`"><span class="gray8">{{ item.title_en }}</span></nuxt-link>
+                </div>
+                <div v-for="level2_item in item.child" :key="level2_item.id" class="mb-3 g-nav">
+                    <div style="margin-bottom:0.45rem;" @click.stop="toggleSiteMap"><nuxt-link :to="`${level2_item.link}`" class="f-85 g-nav-lv2 letter-narrow"> {{ level2_item.title_en }}</nuxt-link></div>
+                    <div v-for="level3_item in level2_item.child" :key="level3_item.id" style="margin-bottom:0.3rem;" @click.stop="toggleSiteMap">
+                        <nuxt-link :to="{ path: level3_item.link, query: { l: is_ENG? 'ENG' : 'KOR' } }" class="f-80 g-nav-lv3 letter-narrow" style="line-height:1!important;"> {{ level3_item.title_en }}</nuxt-link>
+                    </div>
+                </div>
+            </b-col>
+        </b-row>
+    </b-container>
 
 </div>
 </template>
